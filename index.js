c685936abfac44d7cf6b390a1f2efdb28080a4a5
TiagoAzevedo17427 <97109585+TiagoAzevedo17427@users.noreply.github.com>
2022-01-04 19:06:37 +0000

    Add files via upload
    teste 2

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const importData = require('./data.json')
 let port = process.env.PORT || 3000;
 
+app.use(express.json());
 
 
 app.get("/", (req, res) => {
@@ -11,6 +12,33 @@ app.get("/", (req, res) => {
 
 app.get("/players", (req, res) => {
     res.send(importData)
+})
+
+app.get("/players/:name", (req, res) => {
+    const name = importData.find( c => c.name === req.params.name);
+    if(!name) res.status(404).send('does not exist');
+    res.send(name)
+})
+
+app.post("/players", (req, res) => {
+    const name = {
+        name: req.body.name,
+        team: req.body.team,
+        pointsPerGame: req.body.pointsPerGame
+    };
+    importData.push(name);
+    res.send(name);
+
+})
+
+app.delete("/players/:name", (req, res) => {
+    const name = importData.find( c => c.name === req.params.name);
+    if(!name) res.status(404).send('does not exist');
+
+    const deleteName = importData.indexOf(name);
+    importData.splice(deleteName, 1)
+
+    res.send(name);
 })
 
 app.listen(port, () => {
