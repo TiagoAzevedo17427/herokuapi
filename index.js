11057d057166f250dc3ae6142ec612962ebe4f36
TiagoAzevedo17427 <97109585+TiagoAzevedo17427@users.noreply.github.com>
2022-02-09 18:27:42 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ let port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
+
 
 app.get("/", (req, res) => {
     res.send("hello world");
